d3cc3209e4f1b8201414eb70135cc0e58b3a121a
wahyu <wahyu.tohatsu@gmail.com>
2025-11-17 16:05:22 +0900

    Add service worker for caching core assets

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'bento-order-cache-v1';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(k => k !== CACHE_NAME)
+          .map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
